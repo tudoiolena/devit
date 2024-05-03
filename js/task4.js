9edf73837cbbddc49e47876a1c2d1172a5e3930d
Olena Tudoi <olena.tudoi@gmail.com>
2024-05-03 16:36:13 +0300

    Added JS task 4

---
+++ js/task4.js
@@ -0,0 +1,59 @@
+function mapper(rules) {
+  return function (item) {
+    const transformedItem = {};
+    rules.forEach((rule) => {
+      const [fieldName, newFieldName, transformFunction] = rule;
+      const value = item[fieldName];
+      transformedItem[newFieldName] = transformFunction
+        ? transformFunction(value)
+        : value;
+    });
+    return transformedItem;
+  };
+}
+
+let testData3 = [
+  {
+    name: "Vasya",
+    email: "vasya@example.com",
+    age: 20,
+    skills: { php: 0, js: -1, madness: 10, rage: 10 },
+  },
+  {
+    name: "Dima",
+    email: "dima@example.com",
+    age: 34,
+    skills: { php: 5, js: 7, madness: 3, rage: 2 },
+  },
+  {
+    name: "Colya",
+    email: "colya@example.com",
+    age: 46,
+    skills: { php: 8, js: -2, madness: 1, rage: 4 },
+  },
+  {
+    name: "Misha",
+    email: "misha@example.com",
+    age: 16,
+    skills: { php: 6, js: 6, madness: 5, rage: 2 },
+  },
+  {
+    name: "Ashan",
+    email: "ashan@example.com",
+    age: 99,
+    skills: { php: 0, js: 10, madness: 10, rage: 1 },
+  },
+  {
+    name: "Rafshan",
+    email: "rafshan@example.com",
+    age: 11,
+    skills: { php: 0, js: 0, madness: 0, rage: 10 },
+  },
+];
+
+const mapRules = [
+  ["name", "n", (value) => value.toLowerCase()],
+  ["age", "a"],
+];
+
+testData3.map(mapper(mapRules));
